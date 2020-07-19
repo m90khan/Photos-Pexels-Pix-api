e682d79a5f652b4043a8093a73045f30d807d6b4
Khan Mohsin <m90khan@gmail.com>
2020-07-19 19:42:12 +0500

    issue solved : more button

--- index.js
+++ index.js
@@ -5,19 +5,26 @@ const authPexels = "563492ad6f91700001000001545f43d70873455a84aa28d0ea811cbd";
 const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search__input");
 const form = document.querySelector(".search");
+const more = document.querySelector(".more");
 
 let searchValue;
 
-//event listener
+let page = 1;
+let fetchLink;
+let currentSearch;
+//event listeners
 searchInput.addEventListener("input", updateinput);
 function updateinput(e) {
   searchValue = e.target.value;
 }
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  currentSearch = searchValue;
   searchPhotos(searchValue);
 });
 
+more.addEventListener("click", loadMore);
+//API Fetch
 async function fetchApi(url) {
   const datapexel = await fetch(url, {
     //method tell us what we want to do with the data
@@ -28,6 +35,7 @@ async function fetchApi(url) {
     },
   });
   const data = await datapexel.json();
+  console.log(data.photos);
   return data;
 }
 
@@ -35,26 +43,43 @@ function generatePictures(data) {
   data.photos.forEach((photo) => {
     const galleryImg = document.createElement("div");
     galleryImg.classList.add("gallery__img");
-    galleryImg.innerHTML = `<img src=${photo.src.large}> <p>${photo.photographer}</p>`;
+    galleryImg.innerHTML = `
+    <div class="gallery__info  "><p class="btn"><a href="${photo.photographer_url}"target="_blank"  >${photo.photographer}</a></p> 
+    <a href=${photo.src.original} class="btn btn--secondary" target="_blank">Download</a></div> <img src=${photo.src.large}>`;
     gallery.appendChild(galleryImg);
   });
 }
 
 async function pexelsPhotos() {
-  const data = await fetchApi(
-    `https://api.pexels.com/v1/curated?per_page=15&page=1`
-  );
+  fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=1`;
+  const data = await fetchApi(fetchLink);
 
   generatePictures(data);
 }
 
 async function searchPhotos(query) {
-  const data = await fetchApi(
-    `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`
-  );
+  clear();
+  fetchLink = `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`;
+  const data = await fetchApi(fetchLink);
   generatePictures(data);
 }
 
+function clear() {
+  gallery.innerHTML = "";
+  searchInput.value = "";
+}
+
+async function loadMore() {
+  page++;
+  //if value already there
+  if (currentSearch) {
+    fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}+query&per_page=15&page=${page}`;
+  } else {
+    fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=${page}`;
+  }
+  const data = await fetchApi(fetchLink);
+  generatePictures(data);
+}
 // async function pexelsPhotos() {
 //   const datapexel = await fetch(
 //     "https://api.pexels.com/v1/curated?per_page=15&page=1",
